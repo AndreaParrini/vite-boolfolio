739af033ce291aefbef4987f520d3bc514f74e13
AndreaParrini <andrea.parrini26@gmail.com>
2024-06-08 00:40:55 +0200

    remove navbare on header

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -7,15 +7,7 @@ export default {
     <header>
         <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <div class="container">
-                <div class="collapse navbar-collapse d-flex justify-content-between" id="collapsibleNavId">
-                    <a class="navbar-brand text-uppercase" href="#">Boolfolio</a>
-                    <form class="d-flex my-2 my-lg-0">
-                        <input class="form-control me-sm-2" type="text" placeholder="Search" />
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
+                <a class="navbar-brand text-uppercase" href="#">Boolfolio</a>
             </div>
         </nav>
 
