53bf58d86022ceaeeeea9d89fa6dcea178a8bccc
AndreaParrini <andrea.parrini26@gmail.com>
2024-06-08 00:01:49 +0200

    add bootstrap and add section header

---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'SectionHeader'
+}
+</script>
+<template>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="container">
+                <div class="collapse navbar-collapse d-flex justify-content-between" id="collapsibleNavId">
+                    <a class="navbar-brand text-uppercase" href="#">Boolfolio</a>
+                    <form class="d-flex my-2 my-lg-0">
+                        <input class="form-control me-sm-2" type="text" placeholder="Search" />
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+</template>
+
+<style></style>
